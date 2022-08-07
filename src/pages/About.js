f0f93ea8d576a68d2b7a20c1d575b1428b3b2ad3
Olayinka.Dev <aremuolayinka02@gmail.com>
2022-08-07 05:44:16 +0100

    This is a blog template that fetches data from a json-server locally

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum distinctio temporibus tempora incidunt error ea, libero perspiciatis tempore suscipit beatae officia dolorem et nostrum ducimus veritatis eligendi fugit consectetur non!</p>
+      </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Contact.js
@@ -0,0 +1,18 @@
+// import { useLocation } from "react-router-dom";
+
+const Contact = () => {
+
+  // const queryString = useLocation().search
+
+  // const queryParams = new URLSearchParams(queryString)
+  // const name = queryParams.get("name")
+
+    return(
+        <div>
+        <h2>Contact</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum distinctio temporibus tempora incidunt error ea, libero perspiciatis tempore suscipit beatae officia dolorem et nostrum ducimus veritatis eligendi fugit consectetur non!</p>
+      </div>
+    )
+}
+
+export default Contact;
